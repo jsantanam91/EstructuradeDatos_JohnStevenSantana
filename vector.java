7c195c7af197b7d654ba5433eb0383ec1054589e
jsantanam91 <jsantanam91@utp.edu.co>
2017-03-01 19:44:11 -0500

    Add files via upload

---
+++ vector.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package estructuradedatos.array;
+
+/**
+ *
+ * @author utp
+ */
+public class vector {
+    private int tam;
+    private String [] vector;
+    
+    public boolean crearector(){
+        boolean salida=true;
+        if(this.vector==null)
+        {
+           
+        this.vector=new String(this.tam);
+        System.out.println("Se creo");
+        }else
+        {System.out.println("No se creo");
+        salida=false;
+        }      
+    return salida;
+    }
+    
+    public void crearvint buscardato;
+    public static class booleano {
+    public booleano insertardato;
+    public booleano eliminardato;
+    public booleano elimiarposicion;
+    public booleano elimiarvector;
+            
+        public booleano() {
+        }
+    
+
+    /**
+     *
+     */
+   
+    
+}
+}
